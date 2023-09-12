8052e2dbc8facfdefe22311cfb39603a62754c29
Meng Xu <mexu2406@uni.sydney.edu.au>
2023-09-12 13:27:00 +1000

    Add two more components - popup window and upload file button

---
+++ squadron-frontend/app/c4_proj_spec/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+import SquadCustom from "@/ui/squad_custom_popup";
+import VideoUpload from '@/app/c4_proj_spec/video_upload';
+
+
+export default function C4Start() {
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [videoUploadOpen, setVideoUploadOpen] = useState<boolean>(false);
+    return (
+      <div>
+        <Button variant="outlined" onClick={() => setModalOpen(true)}>
+          Create project
+        </Button>
+        <Button variant="outlined" onClick={() => setVideoUploadOpen(true)}>Upload video</Button>
+        {isModalOpen && <SquadCustom closeModal={() => setModalOpen(false)} />}
+        {videoUploadOpen && <VideoUpload onClose={() => setVideoUploadOpen(false)} />}
+      </div>
+    );
+  }
+  
