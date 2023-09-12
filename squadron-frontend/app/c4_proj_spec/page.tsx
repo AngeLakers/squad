732197afd6fdb0d30c7e54b6f915f222b2cd337e
Meng Xu <mexu2406@uni.sydney.edu.au>
2023-09-12 21:41:23 +1000

    Added a invite-admin popup component

--- squadron-frontend/app/c4_proj_spec/page.tsx
+++ squadron-frontend/app/c4_proj_spec/page.tsx
@@ -4,17 +4,20 @@ import Button from '@mui/material/Button';
 import { useState } from 'react';
 import SquadCustom from "@/ui/option-popup";
 import VideoUpload from '@/app/c4_proj_spec/video-upload';
+import InviteAdmin from '@/ui/non-repeated/invite-admin';
 
 
 export default function C4Start() {
     const [isModalOpen, setModalOpen] = useState(false);
     const [videoUploadOpen, setVideoUploadOpen] = useState<boolean>(false);
+    const [isAdminInvite, setAdminInviteOpen] = useState(false);
     return (
       <div>
         <Button variant="outlined" onClick={() => setModalOpen(true)}>
           Create project
         </Button>
         <Button variant="outlined" onClick={() => setVideoUploadOpen(true)}>Upload video</Button>
+        <Button variant="outlined" onClick={() => setAdminInviteOpen(true)}>Add admins</Button>
         {isModalOpen && (
           <SquadCustom
             closeModal={() => {
@@ -30,6 +33,8 @@ export default function C4Start() {
           />
         )}
         {videoUploadOpen && <VideoUpload onClose={() => setVideoUploadOpen(false)} />}
+        {isAdminInvite && <InviteAdmin onClose={() => setAdminInviteOpen(false)} />}
+
       </div>
     );
   }
