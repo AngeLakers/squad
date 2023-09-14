a6802d746e36778a9120110e98d56f39aa478120
Meng Xu <mexu2406@uni.sydney.edu.au>
2023-09-14 12:40:17 +1000

    complete add-role; change upload-file-button as a parameter; fix popup bugs

--- squadron-frontend/ui/non-repeated/add-role.tsx
+++ squadron-frontend/ui/non-repeated/add-role.tsx
@@ -5,6 +5,8 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Slider from '@mui/material/Slider';
 
 const Container = styled.div`
     display: flex;
@@ -46,14 +48,14 @@ const ScrollableContainer = styled.div`
     overflow-y: auto;
     width: 100%;
     margin-top: 2%;
+    height:40vh;
 `;
 
 const RoleDetailsContainer = styled.div`
     background-color: white;
-    margin: 1% 3%;  
+    margin: 2% 3%;  
     padding: 3% 3%; 
     overflow-y: auto;
-    height: 40vh;
     background-color: white;
     border-radius: 10px;
 `;
@@ -65,11 +67,32 @@ const RoleDetailsTitle = styled.h2`
 `;
 
 const RoleTitleLabel = styled.label`
-  display: block;
-  margin-top: 3%;
-  margin-bottom: 2%;
+    display: block;
+    margin-top: 3%;
+    margin-bottom: 2%;
+    color: grey;
+    font-weight: normal; 
+    font-size:0.9rem;
+`;
+
+const OverlapHoursLabel = styled.label`
+    color: grey;
+    font-weight: normal; 
+    font-size:0.9rem;
+`;
+
+const StyledDescription = styled.p`
   color: grey;
-  font-weight: normal; 
+  font-weight: 300;
+  font-size: 0.8rem;
+`;
+
+const LabelsContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin-bottom: 1%;
+    align-items: center; 
 `;
 
 const StyledTextField = styled(TextField)`
@@ -91,42 +114,122 @@ const StyledFormControlLabel = styled(FormControlLabel)`
   }
 `;
 
+const TimeZoneAndOverlapContainer = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    margin-bottom: 1%; 
+`;
+
+const TimeZoneTextField = styled(StyledTextField)`
+    width: 74%; 
+`;
+
+const OverlapTextField = styled(StyledTextField)`
+    width: 24%;
+`;
+
+const Divider = styled.div`
+    width: 100%;
+    height: 1px;
+    background-color: grey;
+    margin-top:3%;
+`;  
+
+const marks = [
+    { value: 5, label: '5' },
+    { value: 10, label: '10' },
+    { value: 15, label: '15' },
+    { value: 20, label: '20' },
+    { value: 25, label: '25' },
+    { value: 30, label: '30' },
+    { value: 35, label: '35' },
+    { value: 40, label: '40' },
+];  
+
+
+function hoursText(value: number) {
+    return `${value}h per week`;
+}
+
 interface AddRoleProps {
-  onClose: () => void;
+    onClose: () => void;
 }
 
 const AddRole: React.FC<AddRoleProps> = ({ onClose }) => {
-  return (
+    return (
     <PopupComponent onClose={onClose}>
-      <Container>
+        <Container>
         <TitleContainer>
         <Title>Add role</Title>
         <Description>Add details about this Squadmate.</Description>
         </TitleContainer>
-   
 
         <ScrollableContainer>
             <RoleDetailsContainer>
                 <RoleDetailsTitle>Role details</RoleDetailsTitle>
                 <RoleTitleLabel>Role title</RoleTitleLabel>
-                <StyledTextField fullWidth  id="fullWidth" select label="Find a role" size="small"/>
-                <StyledFormControlLabel
-                  control={<Checkbox />}
-                  label="Squad Lead"
-                />
-                <p style={{color: 'grey', fontWeight: '300', fontSize: '0.8rem'}}>This role will be a squad leader</p>
+                <StyledTextField fullWidth  id="fullWidth" select size="small">
+                <MenuItem value="Product Manager">Product Manager</MenuItem>
+                <MenuItem value="Software Engineer">Software Engineer</MenuItem>
+                </StyledTextField>
+                <StyledFormControlLabel control={<Checkbox />} label="Squad Lead"/>
+                <StyledDescription>This role will be a squad leader</StyledDescription>
                 <RoleTitleLabel>Role description</RoleTitleLabel>
-                <StyledTextField fullWidth id="multiline-static" multiline rows={4} defaultValue="What are you looking for in this role?"/>
+                <StyledTextField fullWidth id="multiline-static" multiline rows={4}/>
                 <RoleTitleLabel>Hourly rate</RoleTitleLabel>
-                <StyledTextField fullWidth  id="fullWidth" select label="Select hourly rate" size="small"/>
+                <StyledTextField fullWidth  id="fullWidth" select size="small">
+                <MenuItem value="$50-60/h">$50-60/h</MenuItem>
+                <MenuItem value="$80-90/h">$80-90/h</MenuItem>
+                </StyledTextField>
             </RoleDetailsContainer>
+            <RoleDetailsContainer>
+                <RoleDetailsTitle>Skills</RoleDetailsTitle>
+                <RoleTitleLabel>Required skills</RoleTitleLabel>
+                <StyledTextField fullWidth  id="fullWidth" label= "Search skills"size="small"/>
+                <RoleTitleLabel>Preferred skills</RoleTitleLabel>
+                <StyledTextField fullWidth  id="fullWidth" label= "Search skills"size="small"/>
+                <RoleTitleLabel>Tools</RoleTitleLabel>
+                <StyledTextField fullWidth  id="fullWidth" label= "Search tools"size="small"/>
+            </RoleDetailsContainer>
+            <RoleDetailsContainer>
+            <RoleDetailsTitle>Talent location & availability</RoleDetailsTitle>
+            <LabelsContainer>
+            <RoleTitleLabel>Select time zone</RoleTitleLabel>
+            <OverlapHoursLabel>Overlap hours</OverlapHoursLabel>
+            </LabelsContainer>
+            <TimeZoneAndOverlapContainer>
+                <TimeZoneTextField select size="small">
+                <MenuItem value="EST">EST</MenuItem>
+                </TimeZoneTextField>
+                <OverlapTextField select size="small">
+                <MenuItem value="0">0</MenuItem>
+                </OverlapTextField>
+            </TimeZoneAndOverlapContainer>
+            <Divider />
+            <RoleTitleLabel>Location</RoleTitleLabel>
+            <StyledTextField fullWidth  id="fullWidth" select label= "Search location" size="small">
+                <MenuItem value="Sydney">Sydney</MenuItem>
+                <MenuItem value="Brisbane">Brisbane</MenuItem>
+            </StyledTextField>
+            <RoleTitleLabel>Minimum hours per week</RoleTitleLabel>
+            <Slider
+                aria-label="Hours per week"
+                defaultValue={20}
+                getAriaValueText={hoursText}
+                step={1}
+                marks={marks}
+                valueLabelDisplay="on"
+                min={5}
+                max={40}
+            />
+        </RoleDetailsContainer>
         </ScrollableContainer>
- 
         <ButtonContainer>
             <Button variant="outlined" onClick={onClose}>Cancel</Button>
             <Button variant="outlined">Add role</Button>
         </ButtonContainer>
-      </Container>
+        </Container>
     </PopupComponent>
   );
 };
