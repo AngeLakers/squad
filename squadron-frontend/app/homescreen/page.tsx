1ac21f28341e502a605fb461f0cc1012b7c18e76
Bojia Zhang <bzha7778@uni.sydney.edu.au>
2023-10-08 15:50:50 +1100

    Add files via upload

---
+++ squadron-frontend/app/homescreen/page.tsx
@@ -0,0 +1,59 @@
+'use client'
+import Image from 'next/image'
+import styles from './page.module.css'
+import {Application} from "@/ui/Application"
+import { Topbar1 } from "@/ui/Topbar1"
+import {Wallet} from "@/ui/Wallet"
+import {Community} from"@/ui/Community"
+import {Community2} from"@/ui/Community2"
+import {Header} from "@/ui/Header"
+import styled from "styled-components"
+const Display=styled.div`
+display: flex;
+margin-top: 50px;
+align-items: center;
+justify-content: center;
+flex-direction: row;
+`;
+
+const Displayy=styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: row;
+`;
+
+
+export default function Home() {
+  return (
+    <>
+   <Displayy>
+    <Topbar1></Topbar1>
+    </Displayy>
+
+
+    < Displayy>
+    <Header></Header>
+    </Displayy>
+
+    <Application></Application>
+
+    < Displayy>
+    <Wallet></Wallet>
+    </Displayy>
+
+
+    < Display>
+     <Community> </Community>
+    </Display>
+
+    <Display>
+    <Community2>  </Community2>
+    </Display>
+
+</>
+
+
+
+  )
+}
