8728e05da2333e04484307b3de38a05b552a1b37
Meng Xu <mexu2406@uni.sydney.edu.au>
2023-10-12 12:30:56 +1100

    hire squad component

--- squadron-frontend/app/components/page.tsx
+++ squadron-frontend/app/components/page.tsx
@@ -73,6 +73,7 @@ import RequireDontMatchPopup from "@/ui/require-dont-match";
 import ApplyCancelPopup from "@/ui/apply_cancel";
 import BuildApplySquad from "@/ui/build-apply-squad";
 import AskQuestionPopup from "@/ui/ask-question-popup";
+import hideImage from "@/public/hide.png";
 
 const bookData: Array<PersonData> = [
   {
@@ -413,6 +414,7 @@ export default function AllComponents() {
   const [isRoleAdd, setAddRoleOpen] = useState(false);
   const [editprojPopupOpen, setEditprojPopupOpen] = useState(false);
   const [launchMissionPopupOpen, setlaunchMissionPopupOpen] = useState(false);
+  const [hideSquadPopupOpen, setHideSquadPopupOpen] = useState(false);
   const [exitUncompletedPopupOpen, setexitUncompletedPopupOpen] =
     useState(false);
   const [bookInterviewAPopupOpen, setbookInterviewAPopupOpen] = useState(false);
@@ -764,6 +766,17 @@ export default function AllComponents() {
           preset="default"
           label="Launch Mission"
           onClick={() => setlaunchMissionPopupOpen(true)}
+        />
+      </ComponentWrapper>
+      <ComponentWrapper
+        filename="editproj-popup.tsx"
+        createdBy="Becky Xu"
+        description="C5 - Hide Squad Popup"
+      >
+        <CustomButton
+          preset="default"
+          label="Hide Squad"
+          onClick={() => setHideSquadPopupOpen(true)}
         />
       </ComponentWrapper>
       <ComponentWrapper
@@ -1377,6 +1390,26 @@ export default function AllComponents() {
               height="80"
             />
           }
+        />
+      )}
+      {hideSquadPopupOpen && (
+        <EditProjPopup
+          onClose={() => setHideSquadPopupOpen(false)}
+          title="Hide this squad"
+          description="Please, let as not why you don’t want to see this squad. This will help us to give you better options"
+          cancelButtonText="Cancel"
+          confirmButtonText="Hide"
+          showTextarea={true}
+          icon={
+            <Image
+              key="hideImage"
+              src={hideImage}
+              alt="Hide Icon"
+              width="44"
+              height="44"
+            />
+          }
+          //showDivider={false}
         />
       )}
 
