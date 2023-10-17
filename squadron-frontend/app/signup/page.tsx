2342775c9299e1fa9e293a52b64abe31af96564d
Maolin SUN <msun4680@uni.sydney.edu.au>
2023-10-17 21:51:46 +1100

    Create the AuthenticationForm component

--- squadron-frontend/app/signup/page.tsx
+++ squadron-frontend/app/signup/page.tsx
@@ -1,25 +1,20 @@
 "use client";
 import * as React from "react";
 import styled from "styled-components";
-import { ApplicationIntro } from "@/ui/application-intro";
-import { ApplicationSignUp } from "@/ui/application-signup";
+import ApplicationIntro from "@/ui/application-intro";
+import AuthenticationForm from "@/ui/application-auth";
 
 const SignUp = styled.div`
-  align-items: center;
-  text-align: center;
-  color: white;
   display: flex;
   flex-direction: row;
   min-height: 100vh;
-  width: 100%;
-  position: relative;
 `;
 
 export default function Home() {
   return (
     <SignUp>
       <ApplicationIntro />
-      <ApplicationSignUp />
+      <AuthenticationForm Login={false} />
     </SignUp>
   );
 }
