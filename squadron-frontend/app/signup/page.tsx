c01560cc17241dc6664b0cba7fc898cbd7c9a7d9
Maolin SUN <msun4680@uni.sydney.edu.au>
2023-10-31 01:26:14 +1100

    componentize sign up

--- squadron-frontend/app/signup/page.tsx
+++ squadron-frontend/app/signup/page.tsx
@@ -1,20 +1,7 @@
 "use client";
 import * as React from "react";
-import styled from "styled-components";
-import ApplicationIntro from "@/ui/application-intro";
-import AuthenticationForm from "@/ui/application-auth";
-
-const SignUp = styled.div`
-  display: flex;
-  flex-direction: row;
-  min-height: 100vh;
-`;
+import SignUp from "@/ui/signup";
 
 export default function Home() {
-  return (
-    <SignUp>
-      <ApplicationIntro />
-      <AuthenticationForm Login={false} />
-    </SignUp>
-  );
+  return <SignUp login={false} />;
 }
