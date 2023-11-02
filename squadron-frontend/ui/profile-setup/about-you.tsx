3bfe2713d7d3aaaa831f2e8d17311f9ea1add5a0
changxxxx <cguo8976@uni.sydney.edu.au>
2023-11-02 11:42:10 +1100

    [Subjects]: 1. Add Profile nav in t2. 2. Connect experience and projects in t2.

---
+++ squadron-frontend/ui/profile-setup/about-you.tsx
@@ -0,0 +1,6 @@
+
+export default function AboutYou() {
+    return (
+        <div>About You</div>
+    )
+}
---
+++ squadron-frontend/ui/profile-setup/finer-details.tsx
@@ -0,0 +1,6 @@
+
+export default function FinerDetails() {
+    return (
+        <div>Finer Details</div>
+    )
+}
