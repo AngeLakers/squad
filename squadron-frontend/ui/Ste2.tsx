ee5c115adb03638021d30fdc4fa651863b4a82c2
Bojia Zhang <bzha7778@uni.sydney.edu.au>
2023-11-11 11:11:38 +1100

    Update Ste2.tsx

--- squadron-frontend/ui/Ste2.tsx
+++ squadron-frontend/ui/Ste2.tsx
@@ -1,12 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+
 
 import styled from "styled-components"
 import { Divider } from '@mui/material';
 import{useState} from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 
 const Btn=styled.button`
